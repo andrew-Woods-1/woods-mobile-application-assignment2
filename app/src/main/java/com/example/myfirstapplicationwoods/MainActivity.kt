f51f00bd93ca225c34ad4247ce0b5e06f1e69215
Andrew Woods <andrew.woods.1@ndus.edu>
2025-12-02 22:14:25 -0600

    Committing New Permission Added

--- app/src/main/java/com/example/myfirstapplicationwoods/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplicationwoods/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.example.myfirstapplicationwoods
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val MSE_PERMISSION = "com.example.myfirstapplicationwoods.MSE412"
+        private const val REQ_MSE_PERMISSION = 1001
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // XML layout
 
+        // 1) Request the custom permission at runtime when the app starts
+        if (ContextCompat.checkSelfPermission(this, MSE_PERMISSION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(MSE_PERMISSION),
+                REQ_MSE_PERMISSION
+            )
+        }
 
 
         // Display name & ID
         val textView = findViewById<TextView>(R.id.textViewInfo)
         textView.text = "Andrew Woods\nStudent ID: 1356701"
 
-        // Explicit Intent Button
+        // Explicit Intnet Button
         val btnExplicit = findViewById<Button>(R.id.btnExplicit)
         btnExplicit.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
